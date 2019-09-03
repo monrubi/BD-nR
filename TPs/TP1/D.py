dc6334078c2c97124c8d1d5e29afba1f74536077
FranGonRic <45702708+FranGonRic@users.noreply.github.com>
2019-09-03 13:15:21 -0500

    Update D.py

--- TPs/TP1/D.py
+++ TPs/TP1/D.py
@@ -1,9 +1,13 @@
+#Esto imprime la cantidad de diferentes palabras en el archivo de texto dado
 def leerYContarPalabras(archivoTexto):
   f = open(archivoTexto,"r")
   if f.mode=="r":
     cont = f.read()
-    #Convierte los cambios de fila a espacios para que funcione el split
+    #Esta parte convierte a los cambios de fila, los comas y los puntos a espacios
     cont = cont.replace("\n"," ")
+    cont = cont.replace(", "," ")
+    cont = cont.replace(".","")
+    
     lCont = cont.split(" ")
     dic = dict()
     #revisa todas las palabras del texto y la agrega al dictionario si no existen o le suma a la cuenta si sí existen.
@@ -18,5 +22,5 @@ def leerYContarPalabras(archivoTexto):
       count+=1
     print(count)
 
-#El texto es parte de la canción del "Hey Jude" de los Beatles
+#El texto es parte de la canción del "Let it be" de los Beatles
 leerYContarPalabras("textoD.txt")
